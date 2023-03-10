124deb4e0d87bb0fecd6df525e6aa16d3c72122e
RajansinhMakwana <AeGON83@users.noreply.github.com>
2023-03-10 16:59:09 +0530

    Image Index key

--- src/components/ImageIndex.jsx
+++ src/components/ImageIndex.jsx
@@ -1,7 +1,7 @@
 export default function ImageIndex({ data }) {
     const imgArr = data.map((item, index) => {
         return (
-            <div className="ref-img-mini-container">
+            <div className="ref-img-mini-container" key={item.url}>
                 <p className="index-of-img">{index + 1}</p>
                 <img className="ref-img" src={item.url} alt="img not found" ></img>
             </div>
